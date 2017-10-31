3e015c028fa315337ac2501e347675ae2696fe0b
MartianiYuqiong <1329094182@qq.com>
2017-10-31 09:41:40 +0800

    Update download2.cc

--- examples/filetransfer/download2.cc
+++ examples/filetransfer/download2.cc
@@ -26,6 +26,12 @@ void onConnection(const TcpConnectionPtr& conn)
              << " to " << conn->peerAddress().toIpPort();
     conn->setHighWaterMarkCallback(onHighWaterMark, kBufSize+1);
 
+    /**
+      * FILE * fopen(const char * path,const char * mode);
+      * 参数path字符串包含欲打开的文件路径及文件名，
+      * 参数mode字符串则代表着流形态。mode有下列几种形态字符串: 
+      *  rb+ 读写打开一个二进制文件，只允许读写数据
+      */
     FILE* fp = ::fopen(g_file, "rb");
     if (fp)
     {
@@ -57,6 +63,12 @@ void onWriteComplete(const TcpConnectionPtr& conn)
 {
   FILE* fp = boost::any_cast<FILE*>(conn->getContext());
   char buf[kBufSize];
+  /**
+    * size_t fread(void * ptr,size_t size,size_t nmemb,FILE * stream); 
+    * 参数size，表示一次读取的数据单元大小，nmemb表示读取的次数
+    * 成功时fread返回的值与nmemb相等；若小于nmemb但是大于0，则可能是到了文件末尾，不够次数；若返回0，则是文件读取错误，不满一个size的大小。
+    * 所以循环读取文件的时候，while(可以判断是否大于等于0)作为条件，里面再基于feof函数判断是否到了文件末尾而退出循环。
+    */
   size_t nread = ::fread(buf, 1, sizeof buf, fp);
   if (nread > 0)
   {
